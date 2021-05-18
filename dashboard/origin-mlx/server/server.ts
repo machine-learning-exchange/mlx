d2c25c4bfbf562dded311a6f0f01fea2b0a73aa2
Yihong Wang <yh.wang@ibm.com>
2021-05-18 10:45:38 -0700

    Add login for mlx-ui
    Add authentication in `/login` path. Use basic auth and simple
    user model from a configuration in local file system. After a
    successfull login, a cookie will be set in the browser and the user
    info will be store in server. Currently, memorystore is used to
    store session information. we can move it to persistent storage
    later.
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- dashboard/origin-mlx/server/server.ts
+++ dashboard/origin-mlx/server/server.ts
@@ -11,15 +11,23 @@
 // limitations under the License.
 
 import * as express from 'express';
-import {Application, static as StaticHandler} from 'express';
+import * as session from 'express-session';
+import * as fileStore from 'session-file-store';
+import * as passport from "passport";
+import * as cookieParser from "cookie-parser";
+import { BasicStrategy } from "passport-http";
+import { loadUsers, Users } from "./users";
+import { Application, static as StaticHandler } from 'express';
 import * as fs from 'fs';
+import { randomBytes } from 'crypto';
 import * as proxy from 'http-proxy-middleware';
 import * as path from 'path';
 import * as process from 'process';
 
 const {
   MLX_API_ENDPOINT = 'mlx-api',
-  REACT_APP_BASE_PATH = '/mlx'
+  REACT_APP_BASE_PATH = '',
+  SESSION_SECRET = randomBytes(64).toString('hex'),
 } = process.env;
 
 const app = express() as Application;
@@ -32,13 +40,23 @@ const port = process.argv[3] || 3000;
 
 const apiServerAddress = `http://${MLX_API_ENDPOINT}`;
 
-app.all('/' + apiPrefix + '/*', proxy({
-  changeOrigin: true,
-  onProxyReq: proxyReq => {
-    console.log('Proxied request: ', (proxyReq as any).path);
-  },
-  target: apiServerAddress,
-}));
+if (REACT_APP_BASE_PATH.length !== 0) {
+  app.all('/' + apiPrefix + '/*', proxy({
+    changeOrigin: true,
+    onProxyReq: proxyReq => {
+      console.log('Proxied request: ', (proxyReq as any).path);
+    },
+    target: apiServerAddress,
+  }));
+}
+
+type User = {
+  username: string;
+  roles: string[];
+};
+
+// enable session
+initLogin(app);
 
 app.all(REACT_APP_BASE_PATH  + '/' + apiPrefix + '/*', proxy({
   changeOrigin: true,
@@ -65,18 +83,20 @@ app.use(REACT_APP_BASE_PATH + '/delete/', staticHandler);
 app.use(REACT_APP_BASE_PATH + '/kiali/', staticHandler);
 app.use(REACT_APP_BASE_PATH + '/experiments/', staticHandler);
 
-app.use('/', staticHandler);
-app.use('/pipelines/', staticHandler);
-app.use('/components/', staticHandler);
-app.use('/models/', staticHandler);
-app.use('/operators/', staticHandler);
-app.use('/notebooks/', staticHandler);
-app.use('/upload/', staticHandler);
-app.use('/inferenceservices/', staticHandler);
-app.use('/settings', staticHandler);
-app.use('/delete/', staticHandler);
-app.use('/kiali/', staticHandler);
-app.use('/experiments/', staticHandler);
+if (REACT_APP_BASE_PATH.length !== 0) {
+  app.use('/', staticHandler);
+  app.use('/pipelines/', staticHandler);
+  app.use('/components/', staticHandler);
+  app.use('/models/', staticHandler);
+  app.use('/operators/', staticHandler);
+  app.use('/notebooks/', staticHandler);
+  app.use('/upload/', staticHandler);
+  app.use('/inferenceservices/', staticHandler);
+  app.use('/settings', staticHandler);
+  app.use('/delete/', staticHandler);
+  app.use('/kiali/', staticHandler);
+  app.use('/experiments/', staticHandler);
+}
 
 app.get('*', (req, res) => {
   // TODO: look into caching this file to speed up multiple requests.
@@ -86,3 +106,45 @@ app.get('*', (req, res) => {
 app.listen(port, () => {
   console.log('Server listening at http://localhost:' + port);
 });
+
+function initLogin(app: express.Application) {
+  const users = loadUsers();
+
+  passport.serializeUser<User, string>((user: User, done) => {
+    done(undefined, user.username);
+  });
+  passport.deserializeUser<User, string>((username: string, done) => {
+    done(undefined, { username: username, roles: users[username].roles });
+  });
+
+  passport.use(new BasicStrategy(
+    (userid: string, password: string, done: (err: Error, user?: User|boolean) => any) => {
+      if (users[userid] === undefined || users[userid].password !== password) {
+        return done(null, false);
+      }
+      return done(null, { username: userid, roles: users[userid].roles });
+    }
+  ));
+
+  const storeFunc = fileStore(session);
+  // Register related middlewares
+  app.use(cookieParser());
+  const sessOption : session.SessionOptions = {
+    secret: SESSION_SECRET,
+    saveUninitialized: true,
+    cookie: {secure: 'auto', maxAge: 86400000},
+    store: new storeFunc({}),
+    resave: false
+  }
+  app.use(session(sessOption));
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  app.get([REACT_APP_BASE_PATH + '/login'],
+    passport.authenticate('basic'),
+    (req, res) => {
+      res.cookie('userinfo', JSON.stringify(req.user));
+      res.redirect(REACT_APP_BASE_PATH);
+    }
+  );
+}
