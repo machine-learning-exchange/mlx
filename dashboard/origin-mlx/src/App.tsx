40ef366fcd3163bb2b40d6383931f6f41eb7eef6
Yihong Wang <yh.wang@ibm.com>
2021-05-28 21:54:14 -0700

    [mlx-ui] add session-validation
    - Register mlx-ui as an external authorization provider in istio
    - Add session validation API in mlx-ui to wokr as an authorization
      provider
    - Add a profile to represent admin user: mlx@ibm.com
    - Add email as one of user's information
    
    Signed-off-by: Yihong Wang <yh.wang@ibm.com>

--- dashboard/origin-mlx/src/App.tsx
+++ dashboard/origin-mlx/src/App.tsx
@@ -438,7 +438,7 @@ function App() {
                     render={({match, location}) =>
                       <IframePage
                         title="KFP Experiments"
-                        path={KFP + "/pipeline/#/experiments" + window.location.pathname.substring(window.location.pathname.indexOf("/experiments")+12)}
+                        path={KFP + "/_/pipeline/?ns=mlx#/experiments" + window.location.pathname.substring(window.location.pathname.indexOf("/experiments")+12)}
                         storageKey="experiments-iframe"
                       />
                     }
