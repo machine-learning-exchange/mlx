machine-learning-exchange/mlx

commit c183b72d533012530f0ab6d3d29227629c69ae56

[API] Add filter categories (#183)
* Add filter_categories to all assets except Pipelines
* The filter_categories are represented by a Python dictionary
  which is stored in MySQL columns of datatype JSON
* All API list endpoints except pipelines support filtering
  by filter_categories making use of MySQL JSON support
* The catalog list endpoint pushes the filter_categories
  down to the individual list_<asset_type> calls

Resolves #167

Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

api/server/swagger_server/controllers_impl/pipeline_service_controller_impl.py
@@ -192,8 +192,13 @@ def list_pipelines(page_token=None, page_size=None, sort_by=None, filter=None): 
 
     filter_dict = json.loads(filter) if filter else None
 
-    api_pipelines: [ApiPipeline] = load_data(ApiPipelineExtended, filter_dict=filter_dict, sort_by=sort_by,
-                                             count=page_size, offset=offset)
+    # TODO: add filter_categories to ApiPipelineExtension (and give users a way
+    #  to add category labels to pipelines) until then remove categories from filter
+    if "filter_categories" in filter_dict:
+        del filter_dict["filter_categories"]
+
+    api_pipelines: [ApiPipeline] = load_data(ApiPipelineExtended, filter_dict=filter_dict,
+                                             sort_by=sort_by, count=page_size, offset=offset)
 
     next_page_token = offset + page_size if len(api_pipelines) == page_size else None
 

commit f23e8aaaec5b2e6c6b048a5d05095a7242aa476c

[API] Tolerate empty pipeline list filter (#191)
Fix regression introduced in #183

Resolves #191

Signed-off-by: Christian Kadner <ckadner@us.ibm.com>

api/server/swagger_server/controllers_impl/pipeline_service_controller_impl.py
@@ -190,7 +190,7 @@ def list_pipelines(page_token=None, page_size=None, sort_by=None, filter=None): 
     # TODO: do not misuse page_token as MySQL result offset
     offset = int(page_token) if page_token and page_token.isdigit() else 0
 
-    filter_dict = json.loads(filter) if filter else None
+    filter_dict = json.loads(filter) if filter else {}
 
     # TODO: add filter_categories to ApiPipelineExtension (and give users a way
     #  to add category labels to pipelines) until then remove categories from filter