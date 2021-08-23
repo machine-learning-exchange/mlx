76a6332f8523d5d6096314031edddf24a7fd71a7
Andrew-Butler <Andrew.Butler@ibm.com>
2021-08-23 09:54:33 -0700

    Seperate css for md tables vs mlx tables
    Signed-off-by: Andrew-Butler <Andrew.Butler@ibm.com>

--- dashboard/origin-mlx/src/components/MarkdownViewer.tsx
+++ dashboard/origin-mlx/src/components/MarkdownViewer.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import ReactMarkdown from 'react-markdown'
-import remarkToc from 'remark-toc'
+import remarkGfm from 'remark-gfm'
 
 export default class MarkdownViewer extends React.Component<{url: string}, {terms: any}> {
     constructor(props: any) {
@@ -18,7 +18,7 @@ export default class MarkdownViewer extends React.Component<{url: string}, {term
     render() {
       return (
         <div className="content">
-          <ReactMarkdown source={this.state.terms} plugins={[remarkToc]}/>
+          <ReactMarkdown source={this.state.terms} plugins={[remarkGfm]}/>
         </div>
       )
     }
