b21748fec0807951118dd0186771b78e9f89370c
Phu Thai <phuthai450@gmail.com>
2021-10-27 17:16:57 -0400

    Update licensing for outdated files
    Signed-off-by: Phu Thai <phuthai450@gmail.com>
    
    removed bundle license
    
    ignore *.bundle.js
    
    Signed-off-by: Phu Thai <phuthai450@gmail.com>

--- hack/add_license_headers.sh
+++ hack/add_license_headers.sh
@@ -84,7 +84,7 @@ echo "Adding missing license headers"
 find . -type f -not -path '*/temp/*' -a -not -path '*/\.*' -a \( -name '*.py' -o -name '*.yaml' -o -name '*.yml' -o -name '*.sh' \) -exec bash -c 'hash_comment "$0"' {} \;
 
 # Javascript
-find . -type f -not -path '*/node_modules/*' -a -not -path '*/temp/*' -a -not -path '*/\.*' -a \( -name '*.js' -o -name '*.ts' \) -exec bash -c 'slash_comment "$0"' {} \;
+find . -type f -not -path '*/node_modules/*' -a -not -path '*/temp/*' -a -not -path '*/\.*' -a -not -path '*.bundles.js' -a \( -name '*.js' -o -name '*.ts' \) -exec bash -c 'slash_comment "$0"' {} \;
 
 # CSS
 find . -type f -not -path '*/temp/*' -a -not -path '*/\.*' -a \( -name '*.css' -o -name '*.tsx' \) -exec bash -c 'css_comment "$0"' {} \;
--- hack/regenerate_catalog_upload_json.py
+++ hack/regenerate_catalog_upload_json.py
@@ -1,3 +1,7 @@
+  # Copyright 2021 The MLX Contributors
+  #
+  # SPDX-License-Identifier: Apache-2.0
+  
 #!/usr/bin/env python3
 
 # Copyright 2021 IBM Corporation
